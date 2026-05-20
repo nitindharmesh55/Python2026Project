dd086c4347a8e9f2381bfffea00bcdae4feb88b1
Nitin <nitindharmesh01@gmail.com>
2026-05-20 11:49:19 +0530

    First Python

---
+++ main.py
@@ -0,0 +1,73 @@
+name = "nitin"
+age = 23
+
+print("Hello World!") # Hello World!;
+
+# Integer:  A Whole number without a decimal point Example: 10 or -5;
+my_integer_var = 10;
+print('Integer:', my_integer_var);
+
+#float : A Decimal Number Example: 3.14 or -0.001;
+my_float = 3.14;
+print('Float:', my_float);
+
+# string : A sequence of characters enclosed in quotes Example: "Hello" or 'World';
+my_string = "Hello, World!";
+print('String:', my_string);
+
+# Boolean: A data type that can only have two values: True or False;
+my_boolean = True;
+print('Boolean:', my_boolean);
+
+# Sets: An Unordered collection of unique elements
+my_sets = {2.2, 4, "Nitin"};
+print('Sets:', my_sets);
+
+# Dictionaries: A Collection of Key Value Pairs;
+my_dict = {'Namea': 'Nitin', 'Age':23}
+print('Dictionaries:', my_dict);
+
+
+# Tupple: an immutable ordered of collection of elements;
+my_tuple = (1, 2, 3, "Nitin");
+print('Tupple:', my_tuple);
+
+# Range:
+
+# List: An ordered collection of elements that can be changed;
+my_list = [1, 2, 3, "Nitin"];
+print('List:', my_list);    
+
+# string; multi strings;
+Name = """Nitin
+Kumar dharmesh """;
+
+print(Name);
+
+# In Keywords
+
+lime = "Nitin Rajdharmesh is billionaire";
+print("Nitin" in lime);
+
+# Length of the string;
+
+print(len(lime));
+
+# Index [];
+
+print(lime[0]);
+
+
+
+# immutable strings;
+greetings = "hello";
+greetings = "hi";
+print(greetings);
+
+
+# String interpolation; instering variablle or expression inside a string;
+
+name_age = f'my name is {name} and i am {age} years old';
+print(name_age);
+
+# Here f is formate string 
