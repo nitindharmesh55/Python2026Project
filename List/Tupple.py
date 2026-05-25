41e70e8fe1685fab9e5cd5cda05015a31470fe63
Nitin <nitindharmesh01@gmail.com>
2026-05-25 16:26:24 +0530

    tupple

---
+++ List/Tupple.py
@@ -0,0 +1,9 @@
+programming_languages = ('python', 'Javascript', 'java', 'C++')
+# programming_languages[0] = "Javascript" # typeError
+
+# To Access the value from the tupple;
+print(programming_languages[1])
+# To Access the last value you can use the negative index;
+print(programming_languages[-1])
+
+# 
