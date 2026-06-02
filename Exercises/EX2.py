85ddd834e5f00e0583d22d2a5b7bd90431541658
Nitin <nitindharmesh01@gmail.com>
2026-06-02 11:35:46 +0530

    Exersices

---
+++ Exercises/EX2.py
@@ -0,0 +1,121 @@
+for i in range(6):
+    print("Hello")
+
+
+for i in range (1, 6):
+    print(i)
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(2, 11, 2):
+    print(i)
+
+
+for i in range(10,0, -1):
+    print(i)
+
+
+for i in range(5, 31, 5):
+    print(i)
+
+
+for i in range(20, 1, -2):
+    print(i)
+
+for i in range(3, 31, 3):
+    print(i)
+
+for i in  range(50, 4, -5):
+    print(i)
+
+
+for i in range(100, 9, -10):
+    print(i)
+
+
+for i in range(1, 20, 3):
+    print(i)
+
+
+for i in range(30, 2, -3):
+    print(i)
+
+for i in range(2, 31, 4):
+    print(i)
+
+
+for  i in range(100, 9, -15):
+    print((i))
+
+for i in range(7, 71, 7):
+    print(i)
+
+for i in range(50, 0, -7):
+    print(i)
+
+for i in range(1, 20, 2):
+    print(i)
+
+for i in range(60, 5, -6):
+    print(i)
+
+
+for i in range(4 , 41, 4):
+    print(i)
+
+for j in range(25, -1, -5):
+    print(j)
+
+for i in range(5):
+    print("Nitin")
+
+
+name = "Nitin"
+for i in range(3):
+    print(name)
+
+
+for i in range(1, 6):
+    print("Round", i)
+
+for i in range(1, 6):
+    print(i * 2)
+
+
+for i in range(1 , 6):
+    print(i * 3)
+
+
+for i in range(1 , 6):
+    print(i ** 2)
+
+for i in range( 1 , 6):
+    print(i ** 3)
+
+
+for i in range(1 , 6):
+    print(10 + i)
+
+
+for i in range(1 , 6):
+    print(100 - (i * 5))
+
+
+for i in range(1, 6):
+    print(i ** 2 + 1)
+
+
+for i in range ( 1 , 6):
+    print(i ** 2 - 1)
+
+for i in range(1, 6):
+    print(i ** 2 + 2)
+
+
+for i in range(1, 6):
+    print(i * (i + 1))
+
+
+for i in range(1 , 6):
+    print(i)
